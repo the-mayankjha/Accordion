edaffa06685d58763144d7ee44ee2374b173c8f2
flashcodes-themayankjha <the.mayank.k.jha@gmail.com>
2026-02-14 05:17:21 +0530

    FEAT: WPA Icon

--- vite.config.ts
+++ vite.config.ts
@@ -18,12 +18,14 @@ export default defineConfig({
           {
             src: 'logo-192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           },
           {
             src: 'logo-512.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
