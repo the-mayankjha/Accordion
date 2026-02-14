b1b50d22d0b739be47588b9f7ed9e2beadaed413
flashcodes-themayankjha <the.mayank.k.jha@gmail.com>
2026-02-14 20:59:15 +0530

    FIX: Add purpose: maskable to PWA icons

--- vite.config.ts
+++ vite.config.ts
@@ -20,17 +20,20 @@ export default defineConfig({
           {
             src: '/logo-192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           },
           {
             src: '/logo-512.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           },
           {
             src: '/logo.png',
             sizes: '64x64 32x32 24x24 16x16',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
